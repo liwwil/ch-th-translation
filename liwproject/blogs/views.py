fd7d6f8d572a5b7e78d9ab7c981e2786171774f7
“Tanya <cutanyasanganan@gmail.com>
2021-11-06 20:07:08 +0700

    connect database with blog system

--- liwproject/blogs/views.py
+++ liwproject/blogs/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from category.models import Category
 
 # Create your views here.
 def index(request):
-    return render(request, "frontend/index.html")
+    categories = Category.objects.all() 
+    return render(request,"frontend/index.html",{'categories':categories}) 
